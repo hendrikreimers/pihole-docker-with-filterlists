f3ee7405e4129d683d187d240c493bd7495bf728
hendrikreimers <hendrik.reimers@gmail.com>
2023-03-09 16:40:04 +0100

    Scripts optimized

--- bin/reset.sh
+++ bin/reset.sh
@@ -10,10 +10,16 @@ docker image rm hendrikreimers/pihole-lists
 
 rm -Rf $SCRIPTPATH/../etc-dnsmasq.d/*
 rm -Rf $SCRIPTPATH/../etc-pihole/*
-rm -Rf $SCRIPTPATH/../logs/*
+rm -Rf $SCRIPTPATH/../logs-list/*
+rm -Rf $SCRIPTPATH/../logs-pihole/*
 
 cp $SCRIPTPATH/../templates/etc-pihole/* $SCRIPTPATH/../etc-pihole/
 cp $SCRIPTPATH/../templates/etc-dnsmasq.d/* $SCRIPTPATH/../etc-dnsmasq.d/
+
+DIR="$SCRIPTPATH/../_custom"
+if [ -d "$DIR" ]; then
+  cp -Rf $SCRIPTPATH/../_custom/* $SCRIPTPATH/../
+fi
 
 docker system prune -af
 docker builder prune -af
--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ chmod +x $SCRIPTPATH/bin/*.sh
 ./bin/03_setup-system.sh
 ./bin/04_setup-docker-compose.sh
 ./bin/05_pi-save-energy.sh
+./bin/reset.sh
 
 chmod -x $SELF
 
